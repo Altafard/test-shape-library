47d5c1473800c97d4d3fcad1892f909c88b21919
Alexandr Pilipenko <altafard@gmail.com>
2019-05-30 21:58:18 +0300

    Implement client

---
+++ src/ShapeLibrary.Core/Client.cs
@@ -0,0 +1,27 @@
+﻿using ShapeLibrary.Core.Abstractions;
+using ShapeLibrary.Core.Shapes;
+
+namespace ShapeLibrary.Core
+{
+    public class Client : IClient
+    {
+        public double CalculateCircleArea(double radius)
+        {
+            var circle = new Circle(radius);
+            return CalculateShapeArea(circle);
+        }
+
+        public double CalculateTriangleArea(double aSide, double bSide, double cSide)
+        {
+            var triangle = new Triangle(aSide, bSide, cSide);
+            return CalculateShapeArea(triangle);
+        }
+
+        public double CalculateShapeArea(IShape shape) => shape.CalculateArea();
+
+        public bool IsRightTriangle(double aSide, double bSide, double cSide)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
