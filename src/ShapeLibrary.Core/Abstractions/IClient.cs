25b82dd9536e17260e07dcbb5a2e1fac91c54fd6
Alexandr Pilipenko <altafard@gmail.com>
2019-05-30 21:39:58 +0300

    Create facade abstraction

---
+++ src/ShapeLibrary.Core/Abstractions/IClient.cs
@@ -0,0 +1,13 @@
+﻿namespace ShapeLibrary.Core.Abstractions
+{
+    public interface IClient
+    {
+        double CalculateCircleArea(double radius);
+
+        double CalculateTriangleArea(double aSide, double bSide, double cSide);
+
+        double CalculateShapeArea(IShape shape);
+
+        bool IsRightTriangle(double aSide, double bSide, double cSide);
+    }
+}
