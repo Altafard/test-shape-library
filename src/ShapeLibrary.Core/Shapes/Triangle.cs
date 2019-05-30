3e6c8884413fdafab07594e4f0b5946ffb1e4453
Alexandr Pilipenko <altafard@gmail.com>
2019-05-30 22:50:47 +0300

    Extend triangle class with the method which detect if triangle is right

--- src/ShapeLibrary.Core/Shapes/Triangle.cs
+++ src/ShapeLibrary.Core/Shapes/Triangle.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using ShapeLibrary.Core.Abstractions;
 
 namespace ShapeLibrary.Core.Shapes
@@ -27,6 +28,11 @@ namespace ShapeLibrary.Core.Shapes
                 throw new ArgumentException("Side of triangle must be positive value", nameof(cSide));
             }
 
+            if ((aSide + bSide > cSide && bSide + cSide > aSide && cSide + aSide > bSide) == false)
+            {
+                throw new InvalidOperationException("Cannot create triangle with given arguments");
+            }
+
             _aSide = aSide;
             _bSide = bSide;
             _cSide = cSide;
@@ -34,5 +40,26 @@ namespace ShapeLibrary.Core.Shapes
         }
 
         public double CalculateArea() => Math.Sqrt(_p * (_p - _aSide) * (_p - _bSide) * (_p - _cSide));
+
+        public bool HasRightAngle() => HasRightAngle(_aSide, _bSide, _cSide);
+
+        private static bool HasRightAngle(params double[] numbers)
+        {
+            double accumulator = 0;
+
+            double max = numbers.Aggregate((a, b) =>
+            {
+                if (a > b)
+                {
+                    accumulator += b * b;
+                    return a;
+                }
+
+                accumulator += a * a;
+                return b;
+            });
+
+            return Math.Abs(accumulator - max * max) < double.Epsilon;
+        }
     }
 }
